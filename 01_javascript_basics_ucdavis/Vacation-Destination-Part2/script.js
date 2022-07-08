0d346db25bd9752e67ba7d3d9283d776b9702092
Liyon SV <liyon.sv@iqxbusiness.com>
2022-07-09 08:57:35 +1000

    july 9

--- 01_javascript_basics_ucdavis/Vacation-Destination-Part2/script.js
+++ 01_javascript_basics_ucdavis/Vacation-Destination-Part2/script.js
@@ -12,29 +12,37 @@ function handleFormSubmit(event){
     let location       = event.target.elements["location"].value;
     let photo          = event.target.elements["photo"].value;
     let description    = event.target.elements["description"].value;
-    alert("hello");
+
     //clear out the form
     for(let i = 0; i < detailsForm.length ; i++){
         detailsForm.elements[i].value = "";
     }
+
     //create new card
+    let destinationCard = createDestinationCard(name,location,photo,description);
+
     //change header if required
     let destinationContainer = document.getElementById("destinations_container");
     if(destinationContainer.children.length == 0){
         document.getElementById("title").innerHTML = "My Wish List";
     }
+
     //add card
+    document.querySelector("#destinations_container").appendChild(destinationCard);
 
 }
 
 function createDestinationCard(name,location,photoUrl,description){
+
+    debugger;
+
     let card = document.createElement("div");
     card.className = "card";
 
     let img = document.createElement("img");
     img.setAttribute("alt",name);
     
-    let signPost = "images/signpost.jpg";
+    const signPost = "images/signpost.jpg";
 
     if(photoUrl.length == 0){
         img.setAttribute("src",signPost);
@@ -42,6 +50,40 @@ function createDestinationCard(name,location,photoUrl,description){
     else{
         img.setAttribute("src",photoUrl);
     }
+
+    let cardBody = document.createElement("div");
+    cardBody.className = "card-body";
+
+    let cardTitle = document.createElement("h3");
+    cardTitle.innerText = name;
+    cardBody.appendChild(cardTitle);
+
+    let cardSubTitle = document.createElement("h4");
+    cardSubTitle.innerText = location;
+    cardBody.appendChild(cardSubTitle);
+
+    if(description.length != 0){
+        let cardText = document.createElement("p");
+        cardText.className = "card-text";
+        cardText.innerText = description;
+        cardBody.appendChild(cardText);
+    }
+
+    let cardDeleteButton = document.createElement("button");
+    cardDeleteButton.innerText = "Remove";
+
+    cardDeleteButton.addEventListener("click", removeDestination);
+    cardBody.appendChild(cardDeleteButton);
+
+    card.appendChild(img);
+    card.appendChild(cardBody);
+
+    return card;
+}
+
+function removeDestination(event){
+    let card = event.target.parentElement.parentElement;
+    card.remove();
 }
 
 })();
